3ddc60e68a7fb960404a5b5c96e7da76ba963ad2
antmrhdqn <antmrhdqn@gmail.com>
2024-07-07 21:52:21 +0900

    queue1 수정

--- src/study1/queue1.java
+++ src/study1/queue1.java
@@ -5,19 +5,7 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class queue1 {
-
     public static void main(String[] args) {
-        // 정수를 저장하는 큐를 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
-        //
-        //명령은 총 여섯 가지이다.
-        //
-        //push X: 정수 X를 큐에 넣는 연산이다.
-        //pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-        //size: 큐에 들어있는 정수의 개수를 출력한다.
-        //empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
-        //front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-        //back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         sc.nextLine();
@@ -27,16 +15,12 @@ public class queue1 {
         for (int i = 0; i < N; i++) {
             String command = sc.nextLine();
 
-
             switch (command) {
                 case "pop":
-                    try {
-                        int value = queue.remove();
-                        System.out.println(value);
-
-                    } catch (Exception e) {
+                    if (queue.isEmpty()) {
                         System.out.println(-1);
-
+                    } else {
+                        System.out.println(queue.remove());
                     }
                     break;
 
@@ -45,43 +29,37 @@ public class queue1 {
                     break;
 
                 case "empty":
-                    if (queue.isEmpty()) {
-                        System.out.println(1);
-
-                    } else {
-                        System.out.println(0);
-                    }
+                    System.out.println(queue.isEmpty() ? 1 : 0);
                     break;
 
                 case "front":
-                    if (queue.peek() != null) {
-                        System.out.println(queue.peek());
-
-                    } else {
+                    if (queue.isEmpty()) {
                         System.out.println(-1);
-
+                    } else {
+                        System.out.println(queue.peek());
                     }
                     break;
 
                 case "back":
                     if (queue.isEmpty()) {
                         System.out.println(-1);
-
                     } else {
                         System.out.println(((LinkedList<Integer>) queue).getLast());
-
                     }
+                    break;
 
                 default:
-                    if (command.substring(0, 4).equals("push")) {
-                        int X = Integer.parseInt(command.split(" ")[1]);
+                    if (command.startsWith("push")) {
                         try {
+                            int X = Integer.parseInt(command.split(" ")[1]);
                             queue.add(X);
                         } catch (Exception e) {
+                            // push 명령어에서 에러가 발생하면 아무 동작도 하지 않습니다.
                         }
                     }
                     break;
             }
         }
+        sc.close(); // Scanner를 닫습니다.
     }
 }
