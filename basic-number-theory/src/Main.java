0c91fe05097fec14c9b7ada50531737fbb0788e9
bucky1005 <null00007@naver.com>
2024-07-07 11:45:22 +0900

    1. 약수구하기
    ArrayList를 활용한 약수 구하기

--- basic-number-theory/src/Main.java
+++ basic-number-theory/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        getDivisors(n);
+    }
+
+    // 약수 구하기
+    public static int[] getDivisors(int n) {
+        ArrayList answer = new ArrayList();
+
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                answer.add(i);
+            }
+        }
+        System.out.println(answer);
+
+        return answer.stream().mapToInt(x->(int)x).toArray();
     }
 }
