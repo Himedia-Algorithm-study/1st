28c1206786e2bdafae91d4411c8de0ae69b1b064
bucky1005 <null00007@naver.com>
2024-07-08 12:23:54 +0900

    2-1. 소수 - 에라토스테네스의 체
    일정 범위 내에서 소수 구하기

--- basic-number-theory/src/Main.java
+++ basic-number-theory/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -7,7 +8,11 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
-        getDivisors(n);
+        int nums[] = {1,2};
+        System.out.println(nums.length);
+
+//        getDivisors(n);
+        solution(n);
     }
 
     // 약수 구하기
@@ -23,4 +28,43 @@ public class Main {
 
         return answer.stream().mapToInt(x->(int)x).toArray();
     }
+
+    // 에라토스테네스의 체 소수구하기
+    public static int solution(int n) {
+        int answer = 0;
+
+        List<Boolean> primeList = new ArrayList<>(n+1);
+        primeList.add(false);
+        primeList.add(false);
+
+        for(int i=2; i*i<=n; i++){
+            // primeList.get(i)가 true라면,
+            // 그 배수들은 전부 그 숫자를 약수로 가지게 되므로 false 처리
+            if (primeList.get(i)) {
+                for (int j = i * i; j <= n; j+=i) {
+                    primeList.set(j, false);
+                }
+            }
+        }
+
+        List<Integer> temp = new ArrayList<>();
+
+        //PrimeList에서 true인 값들을 숫자 배열로 변환
+        for (int i = 0; i < primeList.size(); i++) {
+            if(primeList.get(i)){
+                temp.add(i);
+            }
+        }
+
+        // 값을 넣을 때마다 answer를 증가시켜 소수의 갯수 카운트
+        int[] primeArray = new int[temp.size()];
+
+        for (int i = 0; i < temp.size(); i++) {
+            primeArray[i] = temp.get(i);
+            answer++;
+        }
+
+        return answer;
+    }
+
 }
